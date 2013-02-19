0b06578b9eea87b130950d45267c8efcf0a92e60
Nikola Mirkov <largeddysimulation@gmail.com>
2013-02-19 09:06:18 +0100

    Added example 1 - two point BVP.

---
+++ examples/ex1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from pysplib import *
+import numpy as np
+from scipy.linalg import solve
+from matplotlib import pyplot as plt
+
+# Constants
+sig1=0.
+sig2=0.
+tau=1.
+ef=1.
+exp=np.exp(1)
+cost=exp/(1.+exp*exp)
+
+# polynomial order
+n=10
+
+# Legendre Gauss-Lobatto nodes
+et,vn=zelegl(n)
+
+# ...and weights
+wt=welegl(et,vn,n)
+
+# Derivative matrix for Legendre at Gauss-Lobatto nodes
+dma=dmlegl(n,et,vn,n)
+
+# Construct differential operator
+l=-np.dot(dma,dma)+tau*np.eye(n+1)
+
+# Change the entries according to BCs
+l[0,:]=0.
+l[n,:]=0.
+l[0,0]=1.
+l[n,n]=1.
+
+# Construction of the rhs vector
+fu=np.zeros(n+1)
+fu[:]=ef
+fu[0]=sig1
+fu[n]=sig2
+
+# Solve the system
+u=np.zeros(n+1)
+u=solve(l,fu)
+
+# Errr vector
+ei=np.exp(et)
+qn=u-(1.-cost*(ei+1./ei))
+
+# Norm of the error vector
+qi,qs,qm = nolegl(vn,qn,wt,n=(len(vn)-1))
+
+# Interpolation to finer grid for plotting
+xx=np.linspace(-1.,1.,100)
+uu = np.polyval(np.polyfit(et,u,n),xx)
+
+# Plot the result
+plt.title('integral err = %e' % qi)
+plt.plot(xx,uu,'b')
+plt.show()
+
